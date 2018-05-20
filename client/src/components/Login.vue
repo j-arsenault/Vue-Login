a802e561a62c57e10343023eb7b2aefdb7c17587
ianarsenault <ian_arsenault@protonmail.com>
2018-05-20 10:08:54 -0400

    8: started initial front-end validation

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -9,33 +9,45 @@
                 <div class="field">
                   <label class="label">Email</label>
                   <div class="control has-icons-left has-icons-right">
-                    <input class="input" type="email" placeholder="joey@google.com">
+                    <input
+                      class="input"
+                      type="email"
+                      v-model="user.email"
+                      v-on:input="checkFormFields"
+                      v-bind:class="{ 'is-danger': errors.email, 'is-danger': error.email }"
+                      placeholder="joey@google.com">
                     <span class="icon is-small is-left">
                         <i class="fas fa-envelope"></i>
                       </span>
-                    <span class="icon is-small is-right">
+                    <span class="icon is-small is-right" v-if="errors.email || error.email">
                         <i class="fas fa-exclamation-triangle"></i>
                       </span>
                   </div>
-                  <p class="help is-danger" v-if="something">This email is invalid</p>
+                  <p class="help is-danger" v-if="error.email">{{error.email}}</p>
                 </div>
                 <div class="field">
                   <label class="label">Password</label>
                   <div class="control has-icons-left has-icons-right">
-                    <input class="input" type="email" placeholder="supersecretpassword">
+                    <input
+                      class="input"
+                      type="email"
+                      v-model="user.password"
+                      v-on:input="checkFormFields"
+                      v-bind:class="{'is-danger': errors.password, 'is-danger': error.password }"
+                      placeholder="supersecretpassword">
                     <span class="icon is-small is-left">
                         <i class="fas  fa-unlock-alt"></i>
                       </span>
-                    <span class="icon is-small is-right">
+                    <span class="icon is-small is-right" v-if="errors.password || error.password">
                         <i class="fas fa-exclamation-triangle"></i>
                       </span>
                   </div>
-                  <p class="help is-danger" v-if="something">This password is invalid</p>
+                  <p class="help is-danger" v-if="error.password">{{error.password}}</p>
                 </div>
 
                 <div class="field is-grouped">
                   <div class="control">
-                    <button class="button is-link">Submit</button>
+                    <button class="button is-link" v-on:click="validateForm()">Submit</button>
                   </div>
                   <div class="control">
                     <button class="button is-text">Cancel</button>
@@ -66,10 +78,40 @@
         },
         passwordsMatch: false,
         errors: {},
-        validUser: false
+        validUser: false,
+        error: {
+          email: '',
+          password: ''
+        }
       }
     },
     methods: {
+      checkFormFields:  _.debounce(function () {
+        console.log('debounce works')
+        if (this.user.email) {
+          this.error.email = ''
+        }
+        if (this.user.password) {
+          this.error.password = ''
+        }
+      }, 500),
+      validateForm() {
+        if (!this.user.email) {
+          this.error.email = 'Email is required'
+        } else {
+          this.error.email = ''
+        }
+        if (!this.user.password) {
+          this.error.password = 'Password is required'
+        } else {
+          this.error.password = ''
+        }
+        if (!this.error.email && !this.error.password) {
+          //this.confirmUser()
+          console.log('Find User by email')
+        }
+
+      },
       async confirmUser() {
         await UserService.fetchUserByEmail(this.user.email).then(res => {
           if (res.data.errors) {
