34f2cc1e454dc310023e0bffcf0e637fe35c829b
ianarsenault <ian_arsenault@protonmail.com>
2018-05-28 00:03:23 -0400

    email verified components

---
+++ client/src/components/EmailVerified.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Email verified component</div>
+</template>
+
+<script>
+  import UserService from '@/services/UserService'
+
+  export default {
+    name: 'EmailVerified',
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
